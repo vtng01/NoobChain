4eb8594ff57b0ba529c4c08b0be36028c98b2d33
vnguy4 <vanthoai.nguyen@aexp.com>
2021-10-28 11:02:26 +0100

    proof of work concept

--- code/src/com/van/Block.java
+++ code/src/com/van/Block.java
@@ -3,10 +3,12 @@ package com.van;
 import java.util.Date;
 
 public class Block {
+
     public String hash;
     public String previousHash;
     private String data;
     private long timeStamp;
+    private int nonce;
 
     public Block(String data, String previousHash) {
         this.data = data;
@@ -19,9 +21,21 @@ public class Block {
         String calculatedHash = StringUtil.applySha256(
                 previousHash +
                         timeStamp +
+                        nonce +
                         data
         );
 
         return calculatedHash;
+    }
+
+    public void mineBlock(int difficulty) {
+        //Create a string with difficulty * "0"
+        String target = new String(new char[difficulty]).replace('\0', '0');
+
+        while(!hash.substring(0, difficulty).equals(target)) {
+            nonce++;
+            hash = calculateHash();
+        }
+        System.out.println("Blocked Mined!: " + hash);
     }
 }
